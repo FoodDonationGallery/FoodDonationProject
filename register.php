7f246e1222618f557b4fa0755102a3c5d693e426
PiyumiParanavithanage <piyumiwathsala310@gmail.com>
2023-12-01 12:19:18 +0530

    register page changed with drop down

--- register.php
+++ register.php
@@ -146,6 +146,7 @@ if(isset($message)){
       <input type="password" name="cpassword" placeholder="Confirm Your Password" required class="box">
       <select name="user_type" class="box">
          <option value="user">User</option>
+         <option value="admin">Admin</option>
       </select>
       <input type="submit" name="submit" value="Register Now" class="btn">
       <p>Already have an account? <a href="login.php">Login Now</a></p>
