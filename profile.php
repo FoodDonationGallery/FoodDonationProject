9e4c029316a64b11a1f687db4fed84f268ef8c16
DulajYapa <116958461+DulajYapa@users.noreply.github.com>
2023-12-03 00:34:35 +0530

    Changes in Navigations

--- profile.php
+++ profile.php
@@ -32,7 +32,7 @@ if(isset($_SESSION['user_id'])){
 
 
     <div class="headers">
-        <a href="" class="logo">
+        <a href="home.php" class="logo">
         <div class="lo">
             <img src="img/logo.jpeg" style="width: 100px; height:65px">
         </div> Food Donation Gallery</a>
